dc9a4757f91e42c060f6253bb0d99c13d64052d4
angelina124 <55613249+angelina124@users.noreply.github.com>
2019-12-01 16:34:02 -0500

    Kush's modifications of the db

--- src/db.py
+++ src/db.py
@@ -4,69 +4,41 @@ from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
-user_water_association_table = Table('user_water_association', db.Model.metadata,
-    Column('user_id', db.Integer, ForeignKey('users.id')),
-    Column('water_id', db.Integer, ForeignKey('water.id'))
-)
-user_fitness_association_table = Table('user_fitness_association', db.Model.metadata,
-    Column('user_id', db.Integer, ForeignKey('users.id')),
-    Column('fitness_id', db.Integer, ForeignKey('fitness.id'))
-)
-user_sleep_association_table = Table('user_sleep_association', db.Model.metadata,
-    Column('user_id', db.Integer, ForeignKey('users.id')),
-    Column('sleep_id', db.Integer, ForeignKey('sleep.id'))
-)
-user_mood_association_table = Table('user_mood_association', db.Model.metadata,
-    Column('user_id', db.Integer, ForeignKey('users.id')),
-    Column('mood_id', db.Integer, ForeignKey('mood.id'))
-)
+#Notes: for now we're internally storing the fitness data as a single int that will indicate
+#intensity and length. Appropriate processing will be done on the backend before
+#returning this data in order to return it as two separate values.
+#Need to ask Conner how to change this to an object containg two properties.
 
+class Datapoints(db.Model):
+    __tablename__='datapoints'
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String, nullable=False)
+    date =db.Column(db.String, nullable=False)
+    water_data=db.Column(db.Integer, nullable=False)
+    mood_data=db.Column(db.Integer, nullable=False)
+    sleep_data=db.Column(db.Integer, nullable=False)
+    fitness_intensity_data=db.Column(db.Integer, nullable=False)
+    fitness_duration_data=db.Column(db.Integer, nullable=False)
 
-class User(db.Model):
-    __tablename__='users'
-    id= db.Column(db.Integer, primary_key=True)
-    username= db.Column(db.String, nullable=False)
-    password_encrypted= db.Column(db.Integer, nullable=False)
-    water = relationship("Water", secondary=user_water_association_table, back_populates="user")
-    fitness = relationship("Fitness", secondary=user_fitness_association_table, back_populates="user")
-    sleep = relationship("Sleep", secondary=user_sleep_association_table, back_populates="user")
-    mood = relationship("Mood", secondary=user_mood_association_table, back_populates="user")
-
+#In reality we will never create a Datapoint object with all the values
+#already filled in. It will always be a blank datapoint object with
+#only the username and date
     def __init__(self, **kwargs):
-        self.username = kwargs.get('username','')
-        #TODO: figure out how we should encrypt passwords
-        self.password_encrypted = kwargs.get('password','')
-
+        self.username = kwargs.get('username')
+        self.date = kwargs.get('date')
+        self.water_data= kwargs.get('water_data',0)
+        self.mood_data= kwargs.get('mood_data',0)
+        self.sleep_data= kwargs.get('sleep_data',0)
+        self.fitness_intensity_data= kwargs.get('fitness_intensity_data',0)
+        self.fitness_duration_data= kwargs.get('fitness_duration_data',0)
 
     def serialize(self):
         return {
-            'id': self.id,
-            'username': self.username
+            'date': self.date,
+            'username': self.username,
+            'water_data': self.water_data,
+            'mood_data': self.mood_data,
+            'sleep_data': self.sleep_data,
+            'fitness_data_intensity': self.fitness_intensity_data,
+            'fitness_data_duration': self.fitness_duration_data
         }
-
-class Datapoint(db.Model):
-    __tablename__='water'
-    id= db.Column(db.Integer, primary_key=True)
-    date=db.Column(db.Integer, nullable=False)  #Unix time
-    amount=db.Column(db.Integer, default=0)
-    user = relationship("User", secondary=user_water_association_table, back_populates="water")
-
-    def __init__(self, **kwargs):
-        self.date = kwargs.get('date','')
-        self.user =  [kwargs.get('user', '')]
-
-
-
-# MODEL POLYMORPHISM: MERGE ALL THESE MODELS INTO A "DAILY" Table
-# - ID, DATE, USER AND THEN COLUMNS FOR THE 4 TYPES
-# ADD ROUTES FOR ALL MY "MOOD" DAYS
-# ADD URL QUERY PARAMETERS FOR FILTERING WHICH TYPE OF DATAPOINT WE WANT (E.G. FILTER BY MOOD)
-# BCRYPT FOR PASSWORD ENCRYPTION
-# LOOK FOR CORRELATIONS
-        # SKLEARN scikit learn
-        # pytorch
-        # pandas
-        # correlation constants b/w -1 and 1
-        # analyze from user's standpoint
-        # full analytics on everyone
-# restrict emojis
